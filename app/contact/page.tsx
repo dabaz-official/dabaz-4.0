e58c9ae3f516ef3fdc477843e07ec7cd7e7b0cae
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-10-27 05:52:38 +0800

    Add pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
-import Footer from './Footer'
-import Navbar from './Navbar'
+import Footer from '../Footer'
+import Navbar from '../Navbar'
 
-export default function Home() {
+export default function Contact() {
   return (
     <div>
       <Navbar />

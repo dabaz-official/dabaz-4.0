3fc9d7df417636a3a45a80946bc88e719ac3072e
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-10-27 23:51:35 +0800

    Update background images

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="bg-neutral-50 dark:bg-neutral-900">
+    <html lang="en" className="bg-[url('/images/light-bg-2.jpg')] bg-cover dark:bg-[url('/images/dark-bg.jpg')]">
       <head>
         <title>DabAZ</title>
         <link rel="icon" href="/logos/primary-logo.svg" />

69b6710540d02b89ed8ca28eb570600c44123dca
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-10-28 00:10:09 +0800

    Update Navbar component

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -22,7 +22,10 @@ export default function Navbar() {
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
         <div className="flex items-center justify-between py-3 md:justify-start md:space-x-6">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <Link href="/" className="flex items-center space-x-3">
+            <Link 
+              href="/" 
+              className="flex items-center space-x-3"
+            >
               <Image 
                 src="/logos/primary-logo.svg"
                 alt="Primary logo"
@@ -46,6 +49,14 @@ export default function Navbar() {
               </Link>
             ))}
           </nav>
+          <div className="items-center justify-end flex flex-1 lg:w-0">
+            <Link 
+              href="/contact" 
+              className="ml-4 md:ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-xl border border-transparent bg-blue-600 px-4 py-2 text-base text-white shadow-sm hover:bg-blue-700 transition duration-200"
+            >
+              Let&apos;s talk
+            </Link>
+          </div>
         </div>
       </div>
     </div>
